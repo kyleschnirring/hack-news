2991564f3f9d1e93fc2829393c68be41d0ed7880
kyleschnirring <kyleschnirring@icloud.com>
2016-02-07 12:57:22 -0800

    removed console.log of headers

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ const https = require('https');
 exports.numberOfTopStories = function (numberOfArticles) {
 
   https.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty', (res) => {
-    console.log('statusCode: ', res.statusCode);
-    console.log('headers: ', res.headers);
-
     res.on('data', (d) => {
       const stories = JSON.parse(d, (key, value) => {
         return value && value.type === 'Buffer'
           ? new Buffer(value.data)
           : value;
       });
-      console.log(stories.splice(0, numberOfArticles));
       var finishedStories = stories.splice(0, numberOfArticles);
       return finishedStories;
     });
@@ -25,16 +21,12 @@ exports.numberOfTopStories = function (numberOfArticles) {
 exports.allTopStories = function () {
 
   https.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty', (res) => {
-    console.log('statusCode: ', res.statusCode);
-    console.log('headers: ', res.headers);
-
     res.on('data', (d) => {
       const stories = JSON.parse(d, (key, value) => {
         return value && value.type === 'Buffer'
           ? new Buffer(value.data)
           : value;
       });
-      console.log(stories);
       return stories;
     });
 
