3f61c73153fb678066cf0d2565975a70536a37d0
kyleschnirring <kyleschnirring@icloud.com>
2016-02-08 15:39:09 -0800

    erased test

--- test.js
+++
@@ -1,9 +0,0 @@
-var serv = require('./index');
-
-serv.numberOfTopStories(10, (numberOfStories) => {
-  console.log(numberOfStories);
-});
-
-serv.allTopStories( (allTheStories) => {
-  console.log(allTheStories);
-});
