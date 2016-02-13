6ba68cb2acd763555fa9fc766ae1296994a099ba
kyleschnirring <kyleschnirring@icloud.com>
2016-02-12 21:30:50 -0800

    i should really make a git ignore

---
+++ README.md
@@ -0,0 +1,51 @@
+## Installation
+
+```javascript
+$ npm install hack-news
+```
+
+## Usage
+
+The examples below shows how to access the IDs of the top stories on Hacker News.
+
+```javascript
+var hn = require('hack-news');
+
+//Returns an array of all the article ids for the top stories on hacker news
+hn.allTopStories( (allTheStories, error) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(allTheStories);
+});
+
+/*Returns an array of article ids in the amount of your choice.
+So if you wanted the top ten stories on Hacker News it would look like this.*/
+hn.numberOfTopStories(10, (numberOfStories, error) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(numberOfStories);
+});
+```
+The examples below shows how to access the IDs of the new stories on Hacker News.
+
+```javascript
+//Returns an array of all the article ids for the new stories on hacker news
+index.allNewStories( (allTheStories, error) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(allTheStories);
+});
+
+/*Returns an array of article ids in the amount of your choice.
+So if you wanted the top ten newest stories on Hacker News it would look like this.*/
+index.numberOfNewStories(10, (numOfStories, error) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(numOfStories);
+});
+```
+This is it for now but stay tuned I will be adding a lot more.
---
+++ index.js
@@ -0,0 +1,81 @@
+const https = require('https');
+
+//get specific number of top stories
+exports.numberOfTopStories = function (numberOfArticles, callback) {
+
+  if (numberOfArticles && typeof numberOfArticles != "number") {
+    throw new Error("The number paramter must be a number");
+  }
+  https.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty', (res) => {
+    res.on('data', (d) => {
+      const stories = JSON.parse(d, (key, value) => {
+        return value && value.type === 'Buffer'
+          ? new Buffer(value.data)
+          : value;
+      });
+      callback(stories.splice(0, numberOfArticles));
+    });
+
+  }).on('error', (e) => {
+    callback(e);
+  });
+}
+
+//get all top stories
+exports.allTopStories = function (callback) {
+
+  https.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty', (res) => {
+
+    res.on('data', (d) => {
+      const stories = JSON.parse(d, (key, value) => {
+        return value && value.type === 'Buffer'
+          ? new Buffer(value.data)
+          : value;
+      });
+      callback(stories);
+    });
+
+  }).on('error', (e) => {
+    callback(e);
+  });
+}
+
+//get specific number of new stories
+exports.numberOfNewStories = function (numberOfArticles, callback) {
+
+  if (numberOfArticles && typeof numberOfArticles != "number") {
+    throw new Error("The number paramter must be a number");
+  }
+  https.get('https://hacker-news.firebaseio.cm/v0/newstories.json?print=pretty', (res) => {
+    res.on('data', (d) => {
+      const stories = JSON.parse(d, (key, value) => {
+        return value && value.type === 'Buffer'
+          ? new Buffer(value.data)
+          : value;
+      });
+      callback(stories.splice(0, numberOfArticles));
+    });
+
+  }).on('error', (e) => {
+    callback(e);
+  });
+}
+
+//get all new stories
+exports.allNewStories = function (callback) {
+
+  https.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty', (res) => {
+
+    res.on('data', (d) => {
+      const stories = JSON.parse(d, (key, value) => {
+        return value && value.type === 'Buffer'
+          ? new Buffer(value.data)
+          : value;
+      });
+      callback(stories);
+    });
+
+  }).on('error', (e) => {
+    callback(e);
+  });
+}
